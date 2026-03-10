213dd3d4dd56e105af440520a49e9d60e1bb5deb
adriano328 <lucasadrianodias@gmail.com>
2026-03-09 20:30:38 -0400

    [feat][Alteralções da tela de cadastro][Alterado os campos de cadastro]

--- src/features/cadastro/useCadastroForm.ts
+++ src/features/cadastro/useCadastroForm.ts
@@ -125,7 +125,7 @@ export function useCadastroForm() {
       senha: formCadastro.senha,
       cpf: formCadastro.cpf.replace(/\D/g, ""),
       campoEclesiastico: {
-        id: formCadastro.campoEclesiasticoId!,
+        id: 1!,
       },
       documento: formCadastro.documento,
     };
